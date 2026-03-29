dae5f5c44130b42330978db3ce618e088cc82c36
Aurel Adestira <aurelsalsaa75@gmail.com>
2026-03-29 21:28:05 +0700

    menambahkan class DataGeneric

--- Aurel Adestira Salsabila tpmodul5_103082400041/Program.cs
+++ Aurel Adestira Salsabila tpmodul5_103082400041/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric h = new HaloGeneric();
+        h.SapaUser<string>("Aurel");
+
+        DataGeneric<string> data = new DataGeneric<string>("103082400041");
+        data.PrintData();
+    }
+}
